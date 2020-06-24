ed2cf386588dfebe73c71cbd1d1fd0ffe6049bd5
AlinaCheban <alinafonat@gmail.com>
2020-06-24 18:51:50 +0300

    Add task0202

---
+++ src/main/java/ua/edu/npu/lab02/task02/Application.java
@@ -0,0 +1,10 @@
+package ua.edu.npu.lab02.task02;
+
+public class Application {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Tim", 8);
+        Dog dog2 = new Dog("Mark", 4);
+        dog1.voice();
+        dog2.voice();
+    }
+}
