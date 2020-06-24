83bda28667391274a055ce5d8be6ad8651a061a9
AlinaCheban <alinafonat@gmail.com>
2020-06-24 18:56:54 +0300

    Add task0203

---
+++ src/main/java/ua/edu/npu/lab02/task03/Application.java
@@ -0,0 +1,10 @@
+package ua.edu.npu.lab02.task03;
+
+public class Application {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Mike", 5);
+        Dog dog2 = new Dog();
+        dog1.showDogData();
+        dog2.showDogData();
+    }
+}
---
+++ src/main/java/ua/edu/npu/lab02/task03/Dog.java
@@ -0,0 +1,29 @@
+package ua.edu.npu.lab02.task03;
+
+public class Dog {
+    int age;
+    String name = "";
+
+    public Dog(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Dog(){
+        name = "Unknown";
+        age = 0;
+    }
+
+    void voice () {
+        System.out.print(name + ": ");
+        for(int i=0; i<age; i++){
+            System.out.print("Bark ");
+        }
+        System.out.println("");
+    }
+
+    public void showDogData () {
+        System.out.println("Name: " + name + "   Age: " + age);
+        voice();
+    }
+}
